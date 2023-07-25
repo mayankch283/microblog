751347e1fbbb6d9387c22f5c480c9e852764e497
mayankch283 <mayankch283@gmail.com>
2023-07-26 00:05:37 +0530

    testing out jinja enabled template engine

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,130 @@
+from flask import render_template
 from app import app
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
-    return "Hello, world!"
+    '''mock objects'''
+    user = {'username': 'Mayank'} 
+    posts = [
+            {
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Oppenheimer movie was so good!'
+
+            },
+            {
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },{
+                'author': {'username' : 'John'},
+                'body': 'Beautiful day in Portland'
+            },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
