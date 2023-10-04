016314d98d2f8df86aa8952787ffbcec225bae34
mayankch283 <mayankch283@gmail.com>
2023-10-05 00:41:02 +0530

    .

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mayankch283/Desktop/microblog/venv"
+set -gx VIRTUAL_ENV "/home/mayankch283/microblog/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
